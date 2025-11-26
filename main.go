2c04b792ee97ba2700281c2a4f955c7c8906af56
manosdetijera <20892112+manosdetijera@users.noreply.github.com>
2025-11-26 09:37:51 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"regexp"
+	"time"
+	"path/filepath"
+	
+	"github.com/kkdai/youtube/v2"
+)
+
+const usage = "Usage: DownloadYTAudio <YouTubeLink>\n" +
+	"Example: DownloadYTAudio https://www.youtube.com/watch?v=INbQpAoaWSw\n\n"
+
+func main() {
+	if len(os.Args) < 2 || len(os.Args) > 2 {
+		fmt.Fprintf(os.Stderr, "Error: Missing required arguments.\n%s", usage)
+		os.Exit(1)
+	}
+
+	videoLink := os.Args[1]
+	regex := regexp.MustCompile(`v=([a-zA-Z0-9_-]*)`)
+	matches := regex.FindStringSubmatch(videoLink)
+
+	if len(matches) < 2 {
+		fmt.Fprintf(os.Stderr, "Error: No video ID in YouTube link.\n%s", usage)
+		os.Exit(1)
+	}
+
+	videoId := matches[1]
+
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(videoId)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to get YouTube link.\n%s", usage)
+		os.Exit(1)
+	}
+
+	formats := video.Formats.WithAudioChannels()
+
+	var targetFormat int = -1
+	for i, format := range formats {
+		if format.MimeType == "audio/mp4; codecs=\"mp4a.40.2\"" {
+			targetFormat = i
+			break
+		}
+	}
+
+	if targetFormat == -1 {
+		fmt.Fprintf(os.Stderr, "Error: No audio channel for YouTube link.\n%s", usage)
+		os.Exit(1)
+	}
+
+	stream, _, err := client.GetStream(video, &formats[targetFormat])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to get stream for YouTube link.\n%s", usage)
+		os.Exit(1)
+	}
+	defer stream.Close()
+
+	filename := fmt.Sprintf("%s.mp4", videoId)
+	today := time.Now().Format("2006-01-02")
+
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to get current user.\n%s", usage)
+		os.Exit(1)
+	}
+
+	// The HomeDir field contains the path to the home directory
+	homeDir := currentUser.HomeDir
+
+	dirPath := filepath.Join(homeDir, "Desktop/YTVideos", today)
+	const dirPerm os.FileMode = 0755
+
+	err = os.MkdirAll(dirPath, dirPerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to create dir on Desktop.\n%s", usage)
+		os.Exit(1)
+	}
+
+	filePath := filepath.Join(dirPath, filename)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to create audio file.\n%s", usage)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Error writing audio file.\n%s", usage)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "Successfully wrote %s\n", filePath)
+}
+
